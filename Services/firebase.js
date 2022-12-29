17de00e61be79d8c7e0300faa84d2ad1546579f5
Manuel Molina Ramirez <manuel.molina.ramirez1991@gmail.com>
2022-12-29 03:33:01 +0100

    bootstrap-formulario

---
+++ Services/firebase.js
@@ -0,0 +1,17 @@
+// Importe las funciones que necesita de los SDK que necesita.
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// TODO: agregar SDK para los productos de Firebase que desea usar.
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// La configuración de Firebase de tu aplicación web.
+const firebaseConfig = {
+  apiKey: "AIzaSyCyoZbt00LYBGwWhzqMp6NYzuuG8WQ_hWY",
+  authDomain: "proyect-new-york-time-firebase.firebaseapp.com",
+  projectId: "proyect-new-york-time-firebase",
+  storageBucket: "proyect-new-york-time-firebase.appspot.com",
+  messagingSenderId: "854473197201",
+  appId: "1:854473197201:web:a04a1f322067928db56130"
+};
+
+// Inicializar Firebase.
+export const app = initializeApp(firebaseConfig);
